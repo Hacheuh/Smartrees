6017e88ff41ad0e4bbb6c483c09b8ff72eb7b611
adam bellity <adambellity@yahoo.fr>
2021-08-29 21:06:33 +0200

    tests for dataframes and dates

--- tests/test_get_dataFrame.py
+++ tests/test_get_dataFrame.py
@@ -24,22 +24,18 @@ def test_base_init():
     assert type(res.scale) == int
 
 def test_get_3bands_df():
-    #res = get_dataFrame.SmarTrees()
     df = res.get_3bands_df()
     assert len(list(df.columns))==3
 
 def test_get_NDVIandKelvin():
-    #res = get_dataFrame.SmarTrees()
     df = res.get_NDVIandKELVIN()
     assert max(df.NDVI)<=1
     assert min(df.B10)>0
 
 def test_display_folium_map():
-    #res = get_dataFrame.SmarTrees()
     maps = res.display_folium_map()
     assert type(maps) == folium.folium.Map
 
 def test_z_temperature():
-    #data = get_dataFrame.SmarTrees()
     assert list(res.z_temperature().columns) == ['Norm_Temp', 'NDVI']
     assert np.unique(res.z_temperature()['Norm_Temp']<10)== True
