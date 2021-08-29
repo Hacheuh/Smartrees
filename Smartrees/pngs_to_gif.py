466e8ecd5d9d63506d21e0ae2f018845ee83048b
Silvia Franze <silvia.franze.521@gmail.com>
2021-08-29 23:09:16 +0200

    packed png_to_gif but doesn't work

--- Smartrees/pngs_to_gif.py
+++ Smartrees/pngs_to_gif.py
@@ -1,7 +1,35 @@
 from PIL import Image
 import glob
+import ee
+from Smartrees.get_dataFrame import SmarTrees
 
-def create_gif(pathname):
+# ee.Initialize()
+# data=SmarTrees()
+# df=data.get_3bands_df()
+
+
+'''add a class to make this code functional???
+'''
+
+
+def create_gif_temp(pathname):
+    '''This function transforms Temp pngs into a gif, and saves it    
+    '''
+    frames = []
+    imgs = glob.glob(pathname+"/*_Temp.png")
+    for i in imgs:
+        new_frame = Image.open(i)
+        frames.append(new_frame)
+    print(frames)
+    # Save into a GIF file that loops forever
+    frames[0].save('png_to_gif.gif', format='GIF',
+                    append_images=frames[1:],
+                    save_all=True,
+                    duration=650, loop=0)
+    return None
+
+
+def create_gif_NDVI(pathname):
     '''This function transforms pngs into a gif, and saves it    
     '''
     frames = []
@@ -12,7 +40,7 @@ def create_gif(pathname):
     print(frames)
     # Save into a GIF file that loops forever
     frames[0].save('png_to_gif.gif', format='GIF',
-                   append_images=frames[1:],
-                   save_all=True,
-                   duration=300, loop=0)
+                    append_images=frames[1:],
+                    save_all=True,
+                    duration=650, loop=0)
     return None
