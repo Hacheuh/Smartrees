56ae433ee6fa1336aeeeb7f06783cfd9829eb548
Silvia Franze <silvia.franze.521@gmail.com>
2021-08-30 12:51:54 +0200

    end gif packaging

--- Smartrees/pngs_to_gif.py
+++ Smartrees/pngs_to_gif.py
@@ -1,16 +1,35 @@
 from PIL import Image
 import glob
 import ee
-from Smartrees.get_dataFrame import SmarTrees
-
-# ee.Initialize()
-# data=SmarTrees()
-# df=data.get_3bands_df()
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-'''add a class to make this code functional???
-'''
+def output_images(df,name,shape):
+    img_B10 = np.array(df['Norm_Temp'])
+    img_NDVI = np.array(df['NDVI'])
+    img_B10 = img_B10.reshape(shape) #  (377, 277) va sostituito con data.shapes['10'][:2]
+    img_NDVI = img_NDVI.reshape(shape) 
+    plt.figure(figsize=(15, 10))
+    plt.imshow(img_B10, cmap='coolwarm')
+    plt.savefig(f'output_images/{name}_Temp.png')
+    plt.close()
+    plt.figure(figsize=(15, 10))
+    plt.imshow(img_NDVI, cmap='RdYlGn')
+    plt.savefig(f'output_images/{name}_NDVI.png')
+    plt.close()
+    return None
 
+def fill_value(data,size,value_ndvi,value_temp):
+    df=pd.DataFrame(data.copy())
+    true_size=len(df)
+    df['indice']=df.index
+    datframe=pd.DataFrame(np.zeros((size,))+value_ndvi,columns={'NDVI'})
+    datframe['Norm_Temp']=np.zeros((size,))+value_temp
+    datframe.loc[df.index,'NDVI']=df.loc[:,'NDVI']
+    datframe.loc[df.index,'Norm_Temp']=df.loc[:,'Norm_Temp']
+    return datframe
 
 def create_gif_temp(pathname):
     '''This function transforms Temp pngs into a gif, and saves it    
@@ -22,7 +41,7 @@ def create_gif_temp(pathname):
         frames.append(new_frame)
     print(frames)
     # Save into a GIF file that loops forever
-    frames[0].save('png_to_gif.gif', format='GIF',
+    frames[0].save('output_gif/temp_gif.gif', format='GIF',
                     append_images=frames[1:],
                     save_all=True,
                     duration=650, loop=0)
@@ -39,8 +58,20 @@ def create_gif_NDVI(pathname):
         frames.append(new_frame)
     print(frames)
     # Save into a GIF file that loops forever
-    frames[0].save('png_to_gif.gif', format='GIF',
+    frames[0].save('output_gif/NDVI_gif.gif', format='GIF',
                     append_images=frames[1:],
                     save_all=True,
                     duration=650, loop=0)
+    return None
+
+def create_gifs_fromdf(datas_obj):
+    dict_df=datas_obj.get_data_from_dates()
+    shapes=datas_obj.shapes[10][:2]
+    i=0
+    for key in dict_df:
+        i=i+1
+        df=fill_value(dict_df[key],shapes[0]*shapes[1],-1, 5)
+        output_images(df, f'Nice_{i}',shapes)
+    create_gif_NDVI('output_images')
+    create_gif_temp('output_images')
     return None
